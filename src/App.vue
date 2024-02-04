f7aa73939988b2c9d16a026e7ae9656b8a34e3bf
carrotFarmer78 <caspardett@gmail.com>
2024-02-04 03:37:38 +0100

    Perfect Project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="card">
+  <div v-if="!isHiddenNo" class="title">
+    💙~ Will you be my valentines? ~💜
+  </div>
+  <div v-else class="title">How could you do this to me?😔 </div>
+
+  <div v-if="!desic" class="buttons">
+    <button @click="toggleYes" class="yesB">Yes 😍😍😍😍</button>
+    <button @click="toggleNo" class="noB">Ew 🤢</button>
+  </div>
+  <img v-if="isHiddenYes" src="../src/gifs/yippie.gif">
+  <img v-if="isHiddenNo" src="../src/gifs/wolf.gif">
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<script setup>
+import {ref} from 'vue';
+const isHiddenYes = ref(false);
+const isHiddenNo = ref(false);
+const desic = ref(false);
+
+function toggleYes(){
+  isHiddenYes.value = !isHiddenYes.value
+  toggledesic()
 }
+function toggleNo(){
+  isHiddenNo.value = !isHiddenNo.value
+  toggledesic()
+}
+function toggledesic(){
+  desic.value = !desic.value
+}
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+body{
+  background-color: pink;
+}
+.title{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 100px;
+  font-size: 90px;
+  font-family: 'Comic Neue', cursive;
+}
+.card{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.buttons{
+  display: flex;
+  justify-content: center;
+}
+button{
+  margin: 30px;
+  padding: 15px 80px;
+
+  border-radius: 25px;
+  font-size: 90px;
+}
+img{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 33%;
 }
 </style>
